2fb22e352a5edd04558dc6039f2f474d978bd001
gigibertolazi <giovana.2023302712@aluno.iffar.edu.br>
2025-04-04 14:29:19 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculador de nota</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form method="POST" action="notasFinais.php">
+        Informe sua média final do semestre:
+            <br><br>
+            <input type="text" name="semestreNota" value="Insira aqui" size="20">
+            <br><br>
+            <input type="submit" value="Calcular">
+
+    </form>
+</body>
+</html>
