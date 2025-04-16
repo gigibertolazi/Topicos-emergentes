64249dfc8aed367a56c12baa6cb537037323c767
gigibertolazi <giovana.2023302712@aluno.iffar.edu.br>
2025-04-16 19:41:31 -0300

    Add files via upload

---
+++ página estilizada/script.js
@@ -0,0 +1,12 @@
+function backAzul(){
+    document.body.style.backgroundColor= "#62a8f8"
+}
+
+function backVerde(){
+    document.body.style.backgroundColor= "#2ddba1"
+}
+
+function backVermelho(){
+    document.body.style.backgroundColor= "#fc5b5b"
+}
+
